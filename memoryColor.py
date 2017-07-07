b0794ea03f42a293abbf73ecdd8e30719942f277
Maykon Oliveira <maykonoliveira850@gmail.com>
2017-07-07 11:02:30 -0300

    Update memoryColor.py

--- memoryColor.py
+++ memoryColor.py
@@ -1,23 +1,71 @@
+import random
+import time
 import pygame
 from pygame.locals import *
 
 pygame.init()
 clock = pygame.time.Clock()
+fontes = pygame.font.SysFont('Arial', 40)
 
 janela = pygame.display.set_mode((800, 400), 0, 32)
 
-background = pygame.Surface(janela.get_size()).convert()
-background.fill((255, 255, 255))
+# Fundo
+background = pygame.image.load('Fundo.png').convert()
 
-circulo_red = pygame.draw.circle(background, (155, 0, 0), (100, 250), 80, 0)
-circulo_yeloow = pygame.draw.circle(background, (155, 155, 0), (300, 250), 80, 0)
-circulo_green = pygame.draw.circle(background, (0, 155, 0), (500, 250), 80, 0)
-circulo_blue = pygame.draw.circle(background, (0, 0, 155), (700, 250), 80, 0)
+# Cores Constantes
+"""VERMELHO = {'apagado': (155, 0, 0), 'aceso': (255, 0, 0)}
+VERDE = {'apagado': (0, 155, 0), 'aceso': (0, 255, 0)}
+AMARELO = {'apagado': (155, 155, 0), 'aceso': (255, 255, 0)}
+AZUL = {'apagado': (0, 0, 155), 'aceso': (0, 0, 255)
 
-while True:
+cores = [VERDE, VERMELHO, AMARELO, AZUL]"""
+
+# Posicoes Possiveis
+posicoes = [(103, 255), (303, 255), (503, 255), (703, 255)]
+
+# Circulos apagadas
+circulo_red = {'cor': [(155, 0, 0), (255, 0, 0)], 'posicao': posicoes[0]}
+circulo_yellow = {'cor': [(155, 155, 0), (255, 255, 0)], 'posicao': posicoes[1]}
+circulo_green = {'cor': [(0, 155, 0), (0, 255, 0)], 'posicao': posicoes[2]}
+circulo_blue = {'cor': [(0, 0, 155), (0, 0, 255)], 'posicao': posicoes[3]}
+
+circulos = [circulo_yellow, circulo_green, circulo_red, circulo_blue]
+
+comecar_text = fontes.render('Comecar', True, (0, 0, 0))
+comecar_rect = comecar_text.get_rect()
+comecar_rect.left = 350
+comecar_rect.top = 80
+
+
+def sequencia_circulo():
+
+    circulo_random = random.choice(circulos)
+    pygame.draw.circle(janela, circulo_random['cor'][1], circulo_random['posicao'], 71, 0)
+    pygame.display.update()
+    time.sleep(2)
+
+jogando = False
+
+while not jogando:
     for evento in pygame.event.get():
         if evento.type == QUIT:
             quit()
+        if evento.type == MOUSEBUTTONDOWN:
+            mouse = pygame.mouse.get_pos()
+            if comecar_rect.collidepoint(mouse):
+                jogando = True
+
+    janela.blit(background, (0, 0))
+    background.blit(comecar_text, (340, 80))
+    pygame.display.update()
+    clock.tick(27)
+
+while jogando:
+    for evento in pygame.event.get():
+        if evento.type == QUIT:
+            quit()
+
+    sequencia_circulo()
 
     janela.blit(background, (0, 0))
     pygame.display.update()
