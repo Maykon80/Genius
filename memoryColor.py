4e626792402250afba6f866f242c7e1426d24b6e
Maykon de Oliveira Mendonça <maykon.oliveira@academico.ifrn.edu.br>
2017-07-13 19:22:18 -0300

    Atualização parte  grafica

--- memoryColor.py
+++ memoryColor.py
@@ -5,10 +5,14 @@ from pygame.locals import *
 
 pygame.init()  # Inicia Pygame
 clock = pygame.time.Clock()  # clock do jogo
-fontes = pygame.font.SysFont('Arial', 40)
+fonte_opcao = pygame.font.SysFont('Arial', 40)
+fonte_status = pygame.font.SysFont('Arial', 20)
 
 janela = pygame.display.set_mode((800, 400), 0, 32)  # Janela
-pygame.display.set_caption("memoryColor")
+# Barra de status para mostrar, imagine o que... os status do jogo!
+barra_status = pygame.Surface((janela.get_width(), 30))
+barra_status.fill((60, 30, 190))  # Cor da barra de status
+pygame.display.set_caption("memoryColor")  # Titulo da janela do jogo
 
 background = pygame.image.load('Fundo.png').convert()  # Fundo
 
@@ -19,48 +23,44 @@ VERDE = (0, 255, 0)
 AZUL = (0, 0, 255)
 
 # Circulos
-circulo_red = {'cor': VERMELHO, 'posicao': (103, 255)}
-circulo_yellow = {'cor': AMARELO, 'posicao': (303, 255)}
-circulo_green = {'cor': VERDE, 'posicao': (503, 255)}
-circulo_blue = {'cor': AZUL, 'posicao': (703, 255)}
+circulo_red = {'cor': VERMELHO, 'posicao': (103, 285)}
+circulo_yellow = {'cor': AMARELO, 'posicao': (303, 285)}
+circulo_green = {'cor': VERDE, 'posicao': (503, 285)}
+circulo_blue = {'cor': AZUL, 'posicao': (703, 285)}
 
 circulos = [circulo_yellow, circulo_green, circulo_red, circulo_blue]  # Lista com os circulos
 
-# Circulos que detectam a escolha
-circulo_detecta_red = pygame.draw.circle(background, (155, 0, 0), (103, 255), 71, 0)
-circulo_detecta_yeloow = pygame.draw.circle(background, (155, 155, 0), (303, 255), 71, 0)
-circulo_detecta_green = pygame.draw.circle(background, (0, 155, 0), (503, 255), 71, 0)
-circulo_detecta_blue = pygame.draw.circle(background, (0, 0, 155), (703, 255), 71, 0)
-
-# Texto começar
-comecar_text = fontes.render('Comecar', True, (0, 0, 0))
-comecar_rect = comecar_text.get_rect()
-comecar_rect.left = 350
-comecar_rect.top = 80
+# Circulos invisiveis que detectam a escolha do usuario
+circulo_detecta_red = pygame.draw.circle(background, (155, 0, 0), (103, 255), 69, 0)
+circulo_detecta_yeloow = pygame.draw.circle(background, (155, 155, 0), (303, 255), 69, 0)
+circulo_detecta_green = pygame.draw.circle(background, (0, 155, 0), (503, 255), 69, 0)
+circulo_detecta_blue = pygame.draw.circle(background, (0, 0, 155), (703, 255), 69, 0)
 
 
-def sequencia_circulo(seq_num):
+# Função para piscar aleatoriamente as cores
+def gerarCoresAleatorias(dificuldade):
     cores_piscadas = []  # Variavel para armazenar as cores que piscaram
 
-    while 1 <= seq_num:
+    while 1 <= dificuldade:
         circulo_random = random.choice(circulos)  # Escolhe a cor para piscar aleatoriamnete
-        print(circulo_random['cor'])
         cores_piscadas.append(circulo_random['cor'])  # Adiciona a cor que piscou à variavel cores_piscadas
+        print(circulo_random['cor'])
         pygame.draw.circle(janela, circulo_random['cor'], circulo_random['posicao'], 71, 0)  # Pisca a cor
         pygame.display.update()
-        time.sleep(2)  # Tempo que a cor fica acesa
+        time.sleep(1.5)  # Tempo que a cor fica acesa
 
-        seq_num -= 1
-        janela.blit(background, (0, 0))  # Limpa a tela
+        dificuldade -= 1
+        janela.blit(background, (0, 30))  # Limpa a tela
         pygame.display.update()
         time.sleep(0.5)  # Tempo que a cor fica apagada
 
     return cores_piscadas
 
 
-def recolhe_resposta(seq_num):
-
-    while 1 <= seq_num:
+# Função para recolher o palpite do jogador
+def recolhe_resposta(dificuldade):
+    resp = []
+    while 1 <= dificuldade:
 
         for event in pygame.event.get():
             if event.type == QUIT:
@@ -68,21 +68,57 @@ def recolhe_resposta(seq_num):
             if event.type == MOUSEBUTTONDOWN:
                 mouse = pygame.mouse.get_pos()
                 if circulo_detecta_red.collidepoint(mouse):
-                    print("Vermelho")
-                    seq_num -= 1
+                    resp.append(VERMELHO)
+                    dificuldade -= 1
                 elif circulo_detecta_yeloow.collidepoint(mouse):
-                    print("Amarelo")
-                    seq_num -= 1
+                    resp.append(AMARELO)
+                    dificuldade -= 1
                 elif circulo_detecta_green.collidepoint(mouse):
-                    print("Verde")
-                    seq_num -= 1
+                    resp.append(VERDE)
+                    dificuldade -= 1
                 elif circulo_detecta_blue.collidepoint(mouse):
-                    print("Azul")
-                    seq_num -= 1
+                    resp.append(AZUL)
+                    dificuldade -= 1
                 else:
-                    print("Clicou fora")
+                    print("Fora!")
 
+    return resp
+
+
+# Função para comparar as cores que piscaram com as que o jogador forneceu
+def confere_resp(seq_aleatoria, resp_jogador):
+    if seq_aleatoria == resp_jogador:
+        return True
+    else:
+        return False
+
+
+# Função para modificar a barra de status
+def muda_status(pontos, dificuldade, texto):
+    barra_status.fill((60, 30, 190))
+
+    pontuacao = fonte_status.render('Pontuação: ' + str(pontos), True, (255, 255, 255))
+    status_dificuldade = fonte_status.render('Dificuldade: ' + str(dificuldade), True, (255, 255, 255))
+    status_txt = fonte_status.render(texto, True, (255, 255, 255))  # Texto para apresentar na barra de status
+
+    barra_status.blit(pontuacao, (10, 4))
+    barra_status.blit(status_dificuldade, (160, 4))
+    barra_status.blit(status_txt, (350, 4))
+
+    janela.blit(barra_status, (0, 0))
+    pygame.display.update()
+
+
+pontos = 0
+dificuldade = 4
 jogando = False
+
+# Textos
+comecar_text = fonte_opcao.render('Começar', True, (0, 0, 0))  # Botao começar
+comecar_rect = comecar_text.get_rect()
+comecar_rect.left = 350
+comecar_rect.top = 80
+texto = ' '  # Textos
 
 # Aguarda o começo do jogo
 while not jogando:
@@ -94,28 +130,40 @@ while not jogando:
             if comecar_rect.collidepoint(mouse):
                 jogando = True
 
-    janela.blit(background, (0, 0))
+    muda_status(pontos, dificuldade, "Decore as cores que piscarem")
+    janela.blit(barra_status, (0, 0))
+    janela.blit(background, (0, 30))
     janela.blit(comecar_text, (340, 80))
     pygame.display.update()
     clock.tick(27)
 
-janela.blit(background, (0, 0))  # Limpa a tela antes de piscar as cores
+janela.blit(background, (0, 30))  # Limpa a tela antes de piscar as cores
 pygame.display.update()
 
 # Loop principal do jogo
 while jogando:
-    pygame.display.set_caption("Decore a sequencia")
-    time.sleep(2)  # Tempo para as cores comecarem a piscar
+    muda_status(pontos, dificuldade, "Decore as cores que piscarem")  # Muda os status
+    time.sleep(2.5)  # Tempo para as cores começarem a piscar
 
     for event in pygame.event.get():
         if event.type == QUIT:
             quit()
 
-    sequencia_circulo(4)
-    pygame.display.set_caption("Refaça a sequencia")
-    recolhe_resposta(4)
-    quit()
+    cores_aleatorias = gerarCoresAleatorias(dificuldade)  # Armazena as cores geradas na variavel
 
-    janela.blit(background, (0, 0))
+    muda_status(pontos, dificuldade, "Repita na ordem certa as cores que acenderam")
+
+    resp_jogador = recolhe_resposta(dificuldade)    # Recolhe a resposta do jogador
+
+    if confere_resp(cores_aleatorias, resp_jogador):  # Verifica se o jogador acertou
+
+        pontos += 100  # Acrescenta mais 100 aos pontos
+
+        if (pontos % 500 == 0) and (pontos != 0):   # Verifica se pode aumentar a dificuldade
+            dificuldade += 1
+    else:
+        quit()
+
+    janela.blit(background, (0, 30))
     pygame.display.update()
     clock.tick(27)
